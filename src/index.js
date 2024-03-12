7dc578d922eb2cebc3192d44c3cba0618c3cd1e8
Kate Lormor <kate.lormor@googlemail.com>
2024-03-12 11:35:46 +1100

    Added current location

--- src/index.js
+++ src/index.js
@@ -24,21 +24,24 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-    <div class="city">
-    <div>
-    <h2>${cityName}</h2>
-    <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss [<small>]a[</small>]")}</div>
-    </div> 
-    `;
+        <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]a[</small>]"
+        )}</div>
+            </div> 
+            `;
 }
 
 let citySelectElement = document.querySelector("#selectCity");
 citySelectElement.addEventListener("change", updateCity);
-
-setInterval(updateCity, 1000);
